68bcf990ec2e8daab49d1959e03278e562363201
Adam Grodahl <adamgrodahl@Adams-MBP.hsd1.or.comcast.net>
2019-09-19 15:43:21 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const path = require('path');
+const members = require('./members');
+const logger = require('./middleware/logger');
+
+const app = express();
+
+//init middleware
+// app.use(logger);
+
+//gets all members
+app.get('/api/members', (req, res) => {
+    res.json(members);
+});
+
+//get single member
+app.get('/api/members/:id', (req, res) => {
+    const found = members.some(member => member.id === parseInt(req.params.id));
+
+    if(found) {
+        res.json(members.filter(member => member.id === parseInt(req.params.id)));
+    } else {
+        res.status(400).json({ msg: `No member with the id of ${req.params.id}` });
+    }
+
+});
+
+//set static foler
+app.use(express.static(path.join(__dirname, 'public')));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log('server started on port ${PORT}')); 
